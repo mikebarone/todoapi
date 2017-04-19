a3e1bb61eca6c206c19dfb496d823a5c047b15a4
mikebarone <mikebarone@gmail.com>
2017-04-19 15:17:34 -0400

    Config

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,11 @@
 var mongoose = require('mongoose');
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://todoapp:todoapp@ds163340.mlab.com:63340/todoapp'
-};
+// let db = {
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   mlab: 'mongodb://todoapp:todoapp@ds163340.mlab.com:63340/todoapp'
+// };
 
 mongoose.Promise = global.Promise;
-mongoose.connect( process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect( process.env.MONGODB_URI );
 
 module.exports = {mongoose};
