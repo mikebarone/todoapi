24ad9a27385abad13714adf161f91fc1baa254a3
mikebarone <mikebarone@gmail.com>
2017-04-18 23:09:43 -0400

    mlab link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 let db = {
   localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: ''
+  mlab: 'mongodb://todoapp:todoapp@ds163340.mlab.com:63340/todoapp'
 };
 
 mongoose.Promise = global.Promise;
