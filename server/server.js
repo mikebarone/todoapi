0ab952e08a2e1e9e9d8cd76f7bb950333d4c86b8
mikebarone <mikebarone@gmail.com>
2017-04-19 19:32:06 -0400

    Authentication

--- server/server.js
+++ server/server.js
@@ -9,11 +9,14 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
 
 app.use(bodyParser.json());
+
+// POST TODO
 
 app.post('/todos', (req,res) => {
     var todo = new Todo({
@@ -26,6 +29,8 @@ app.post('/todos', (req,res) => {
         res.status(400).send(e);
     });
 });
+
+// GET TODO
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
@@ -52,6 +57,8 @@ app.get('/todos/:id', (req, res) => {
     });
 });
 
+// DELETE
+
 app.delete('/todos/:id', (req, res) => {
     var id = req.params.id;
     
@@ -69,6 +76,8 @@ app.delete('/todos/:id', (req, res) => {
     });
 
 });
+
+// UPDATE
 
 app.patch('/todos/:id', (req,res) => {
     var id = req.params.id;
@@ -95,6 +104,8 @@ app.patch('/todos/:id', (req,res) => {
     });
 });
 
+// POST / users
+
 app.post('/users', (req,res) => {
     var user = new User(_.pick(req.body, ['email','password']));
     
@@ -106,6 +117,10 @@ app.post('/users', (req,res) => {
         var err = _.pick(e, ['message','errors.email.message','errors.password.message'])
         res.status(400).send(err);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
